fa3dbb0dd9d2e5d9a4bcde71f85e95234cd87f11
izabelarc <izabelainfinity@gmail.com>
2023-09-15 15:46:42 -0300

    teste

--- aula6/apiTools.py
+++ aula6/apiTools.py
@@ -4,9 +4,7 @@ def get_by_id(id, lista_clientes):
     for cliente in lista_clientes:
         if cliente["id"] == id:
             return cliente
-        else:
-            return f"Não foi possível encontrar o cliente."
-
+    return "Não foi possível encontrar o cliente."
 
 def create_cliente_new(nombre, plato_favorito, conta, lista_clientes: list):
     new_cliente = {
@@ -14,63 +12,48 @@ def create_cliente_new(nombre, plato_favorito, conta, lista_clientes: list):
         "nombre": nombre,
         "plato_favorito": plato_favorito,
         "estrela": 1,
-        "desconto_3_estrelas": 0, #valor da conta com desconto 3 por cento, quando ja tem 3 estrelas
-        "vindas":[
-                {
-                     "data": date.today(),
-                    "conta": conta,
-                },
+        "desconto_3_estrelas": 0,
+        "vindas": [
+            {
+                "data": date.today(),
+                "conta": conta,
+            },
         ]
     }
     lista_clientes.append(new_cliente)
     return lista_clientes
-
 
 def update_cliente_by_id(nombre, plato_favorito, id, lista_clientes):
     for cliente in lista_clientes:
         if cliente["id"] == id:
             cliente["nombre"] = nombre
             cliente["plato_favorito"] = plato_favorito
-
     return lista_clientes
 
-def delete(id, nombre, plato_favorio, lista_clientes):
-    if id in lista_clientes:
-        del lista_clientes[id]
-        new_lista = {}
-       
-        acc = 1
-        for clientes_id in lista_clientes:
-            new_lista[acc] = lista_clientes[clientes_id]
-            acc += 1
-        cursos = new_lista
+def delete(id, lista_clientes):
+    for index, cliente in enumerate(lista_clientes):
+        if cliente["id"] == id:
+            del lista_clientes[index]
+            return lista_clientes
 
-def contador_vindas(lista_clientes,id):
-    for i in lista_clientes:
-        if i["id"] == id:
-            if len(i["vindas"]) >= 3:
-                vindas = i["vindas"]
+def contador_vindas(lista_clientes, id):
+    for cliente in lista_clientes:
+        if cliente["id"] == id:
+            if len(cliente["vindas"]) >= 3:
+                vindas = cliente["vindas"]
                 ultima_vinda = vindas[-1]
                 conta = ultima_vinda["conta"]
-                total =  (1-0.03)*conta
+                total = (1 - 0.03) * conta
+                cliente["desconto_3_estrelas"] = total
                 return total
             else:
-                falta = 3- len(i["vindas"])
+                falta = 3 - len(cliente["vindas"])
                 return f"Faltam {falta} visitas para aplicar o desconto."
-            
+
 def new_conta(id, conta, lista_clientes):
-    for i in lista_clientes:
-        if i["id"] == id:
-            i["vindas"].append({"data":date.today(),"conta":conta})
+    for cliente in lista_clientes:
+        if cliente["id"] == id:
+            cliente["vindas"].append({"data": date.today(), "conta": conta})
+            cliente["estrela"] = len(cliente["vindas"])
     return lista_clientes
-            # i["estrela"] = len(i["vindas"])
-            # if len(i["vindas"]) >= 3:
-            #     vindas = i["vindas"]
-            #     ultima_vinda = vindas[-1]
-            #     conta = ultima_vinda["conta"]
-            #     total =  (1-0.03)*conta
-            #     i["desconto_3_estrelas"] = total
-            #     return lista_clientes
-            # else:
-            #     falta = 3- len(i["vindas"])
-            #     return lista_clientes
+
--- aula6/main.py
+++ aula6/main.py
@@ -15,39 +15,27 @@ async def get_clientes():
             detail="Clientes não encontrados"
         )
 
-
-#Traz cliente através da busca pelo id
+# Traz cliente através da busca pelo id
 @app.get("/clientes/{id}", status_code=status.HTTP_200_OK)
 async def get_cliente_id(id: int):
     try:
         resposta = get_by_id(id, lista_clientes)
-        return resposta
+        if resposta:
+            return resposta
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Cliente não encontrado"
+            )
     except:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Cliente não encontrado"
         )
 
-#Traz cliente através da busca pelo id usando Path
-@app.get('/cliente/{id}', status_code=status.HTTP_200_OK)
-async def get_cliente_id(id: int = Path(
-    title='ID do curso',
-    description=f'Deve ser de 1 a {len(lista_clientes)}',
-    gt=1,
-    lt=len(lista_clientes) + 1)
-    ):
-    try:
-        resposta = get_by_id(id, lista_clientes)
-        return resposta
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail='Cliente não encontrado'
-        )
-
-#Cria um novo cliente
+# Cria um novo cliente
 @app.post("/clientes-create", status_code=status.HTTP_201_CREATED)
-async def create_cliente(nombre: str, plato_favorito: str, conta:float):
+async def create_cliente(nombre: str, plato_favorito: str, conta: float):
     try:
         resposta = create_cliente_new(nombre, plato_favorito, conta, lista_clientes)
         return resposta
@@ -56,41 +44,46 @@ async def create_cliente(nombre: str, plato_favorito: str, conta:float):
             status_code=status.HTTP_409_CONFLICT,
             detail="Não foi possível criar o cliente."
         )
-  
-#Atualiza os dados de um cliente      
+
+# Atualiza os dados de um cliente
 @app.put("/clientes-update/{id}", status_code=status.HTTP_202_ACCEPTED)
 async def update_cliente(nombre: str, plato_favorito: str, id: int):
     try:
         resposta = update_cliente_by_id(nombre, plato_favorito, id, lista_clientes)
-        return resposta
+        if resposta:
+            return resposta
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Cliente não encontrado"
+            )
     except:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
             detail="Não foi possível atualizar o cliente."
         )
 
-#Deleta um cliente e seus dados e atualiza o id dos outros que ficaram
+# Deleta um cliente e seus dados e atualiza o id dos outros que ficaram
 @app.delete("/clientes-delete/{id}", status_code=status.HTTP_202_ACCEPTED)
 async def delete(id: int):
     try:
-        id_int = int(id)
-        lista_clientes.pop(id_int - 1)
-
-        news_ids = 0
-        for cliente in lista_clientes:
-            news_ids += 1
-            cliente["id"] = news_ids
-
-        return lista_clientes
+        resposta = delete(id, lista_clientes)
+        if resposta:
+            return resposta
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Cliente não encontrado"
+            )
     except:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
             detail="Não foi possível deletar o cliente."
         )
 
-
+# Retorna o número de vindas e, se aplicável, o valor da conta com desconto
 @app.get("/vindas/{id}", status_code=status.HTTP_200_OK)
-async def get_conta(id:int):
+async def get_conta(id: int):
     try:
         resposta = contador_vindas(lista_clientes, id)
         return resposta
@@ -100,13 +93,18 @@ async def get_conta(id:int):
             detail="Não foi possível encontrar o número de vindas desse cliente"
         )
 
-
-@app.get("/conta/{id}", status_code=status.HTTP_102_PROCESSING) 
-async def get_conta(id: int, conta: int):
+# Registra uma nova visita para um cliente
+@app.post("/conta/{id}", status_code=status.HTTP_201_CREATED)
+async def new_conta(id: int, conta: float):
     try:
-        resposta = new_conta(id)
-        return resposta
-        
+        resposta = new_conta(id, conta, lista_clientes)
+        if resposta:
+            return resposta
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Cliente não encontrado"
+            )
     except:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
